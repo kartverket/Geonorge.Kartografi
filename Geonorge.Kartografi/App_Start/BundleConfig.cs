5109199d69fee2007756969e1befe19daa2ebd66
Dag Olav <dagolav@arkitektum.no>
2016-12-23 10:08:31 +0100

    Added layout

--- Geonorge.Kartografi/App_Start/BundleConfig.cs
+++ Geonorge.Kartografi/App_Start/BundleConfig.cs
@@ -8,24 +8,16 @@ namespace Geonorge.Kartografi
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new StyleBundle("~/Content/bower_components/kartverket-felleskomponenter/assets/css/styles").Include(
+                "~/Content/bower_components/kartverket-felleskomponenter/assets/css/vendor.min.css",
+                "~/Content/bower_components/kartverket-felleskomponenter/assets/css/vendorfonts.min.css",
+                "~/Content/bower_components/kartverket-felleskomponenter/assets/css/main.min.css"
+            ));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/Content/bower_components/kartverket-felleskomponenter/assets/scripts").Include(
+               "~/Content/bower_components/kartverket-felleskomponenter/assets/js/vendor.min.js",
+               "~/Content/bower_components/kartverket-felleskomponenter/assets/js/main.js"
+           ));
         }
     }
 }
