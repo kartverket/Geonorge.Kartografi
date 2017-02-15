81be0a805c4c0e645ffdc4776fbcfa45bbabd1a9
Dag Olav <dagolav@arkitektum.no>
2017-02-15 14:47:23 +0100

    Fix for url

--- Geonorge.Kartografi/Models/CartographyFile.cs
+++ Geonorge.Kartografi/Models/CartographyFile.cs
@@ -121,7 +121,8 @@ namespace Geonorge.Kartografi.Models
         {
             return HttpContext.Current.Request.Url.Scheme + System.Uri.SchemeDelimiter
                  + HttpContext.Current.Request.Url.Host +
-                 (HttpContext.Current.Request.Url.IsDefaultPort ? "" : ":" + HttpContext.Current.Request.Url.Port);
+                 (HttpContext.Current.Request.Url.IsDefaultPort ? "" : ":" + HttpContext.Current.Request.Url.Port)
+                 +(!HttpContext.Current.Request.Url.Host.Contains("localhost") ? "/kartografi" : "");
         }
     }
 }
