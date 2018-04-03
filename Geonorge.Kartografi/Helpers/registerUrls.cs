6a97383ad4bf0365600c8b25f13a9955f671822a
Benjamin Dehli <benjamin@arkitektum.no>
2018-04-03 09:01:49 +0200

    Add culture condition for GeonorgeUrl helper

--- Geonorge.Kartografi/Helpers/registerUrls.cs
+++ Geonorge.Kartografi/Helpers/registerUrls.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using Geonorge.Kartografi.Models.Translations;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Configuration;
 using System.Web.Mvc;
@@ -10,7 +11,12 @@ namespace Geonorge.Kartografi.Helpers
 
         public static string GeonorgeUrl(this HtmlHelper helper)
         {
-            return WebConfigurationManager.AppSettings["GeonorgeUrl"];
+            var url = WebConfigurationManager.AppSettings["GeonorgeUrl"];
+            var culture = CultureHelper.GetCurrentCulture();
+            if (culture != Culture.NorwegianCode)
+                url = url + Culture.EnglishCode;
+
+            return url;
         }
 
         public static string RegistryUrl(this HtmlHelper helper)
