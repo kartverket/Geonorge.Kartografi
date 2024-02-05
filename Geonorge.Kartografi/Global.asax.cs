d92ba5e4fa2cab4c10d2337f6b79927a300c333a
Dag Olav Dahle <dagolav@arkitektum.no>
2024-02-05 12:41:56 +0100

    Bugfix

--- Geonorge.Kartografi/Global.asax.cs
+++ Geonorge.Kartografi/Global.asax.cs
@@ -14,6 +14,7 @@ using System.Web.Optimization;
 using System.Web.Routing;
 using Geonorge.Kartografi.Models.Translations;
 using Geonorge.Kartografi.Helpers;
+using System.Collections.Specialized;
 
 namespace Geonorge.Kartografi
 {
@@ -52,6 +53,9 @@ namespace Geonorge.Kartografi
 
         protected void Application_BeginRequest()
         {
+
+            ValidateReturnUrl(Context.Request.QueryString);
+
             var cookie = Context.Request.Cookies["_culture"];
             var userAgent = Context.Request.UserAgent;
 
@@ -87,6 +91,26 @@ namespace Geonorge.Kartografi
                 var culture = new CultureInfo(cookie.Value);
                 Thread.CurrentThread.CurrentCulture = culture;
                 Thread.CurrentThread.CurrentUICulture = culture;
+            }
+        }
+
+        void ValidateReturnUrl(NameValueCollection queryString)
+        {
+            if (queryString != null)
+            {
+                var returnUrl = queryString.Get("returnUrl");
+                if (!string.IsNullOrEmpty(returnUrl))
+                {
+                    returnUrl = returnUrl.Replace("http://", "");
+                    returnUrl = returnUrl.Replace("https://", "");
+
+                    var host = Request.Url.Host;
+                    if (returnUrl.StartsWith("localhost:44353"))
+                        host = "localhost";
+
+                    if (!returnUrl.StartsWith(host))
+                        HttpContext.Current.Response.StatusCode = 400;
+                }
             }
         }
     }
